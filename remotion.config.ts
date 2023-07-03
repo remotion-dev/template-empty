d8692d470c675079dd01eb81b09840000a141abe
Jonny Burger <jonathanburger11@gmail.com>
2023-07-03 11:59:06 +0200

    Update remotion.config.ts

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,4 +1,9 @@
-// Note: When using the Node.JS APIs, the config file doesn't apply. Instead, pass options directly to the APIs
+/**
+ * Note: When using the Node.JS APIs, the config file
+ * doesn't apply. Instead, pass options directly to the APIs.
+ * 
+ * All configuration options: https://remotion.dev/docs/config
+ */
 
 import {Config} from '@remotion/cli/config';
 
