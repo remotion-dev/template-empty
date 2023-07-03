4aeeef7a0b197fcac9dd15f53c5b3b5529cfa068
MehmetAdemi <mehmet@remotion.dev>
2023-07-03 09:54:08 +0200

    upgraded & checked

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,4 +1,6 @@
-import { Config } from "@remotion/cli/config";
+// Note: When using the Node.JS APIs, the config file doesn't apply. Instead, pass options directly to the APIs
 
-Config.setVideoImageFormat("jpeg");
+import {Config} from '@remotion/cli/config';
+
+Config.setVideoImageFormat('jpeg');
 Config.setOverwriteOutput(true);
